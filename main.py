47ac16ffc980e8c2b39be964c77e229a41548ea5
Julian Canepa <julian.l.canepa@gmail.com>
2024-11-12 15:50:46 -0700

    Small tweaks of main file for user experience.

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ issuer = "OSU CS370 OTP App"
 secret_key = 'JD3XNKOGKREHUZLZVZHYKUXXBZQSGPNV' # pyotp.random_base32()
 
 def generate_qr(user, issuer, secret):
-    # Create the URI expected by Google Authenticator
+    # create URI expected by Google Authenticator
     uri = pyotp.totp.TOTP(secret).provisioning_uri(
         name=user,
         issuer_name=issuer)
 
-    # Generate the QR codex
+    # generate the QR codex
     qr = qrcode.make(uri)
 
+    # create image of qr code, and share it with the user
     filename = 'qr.png'
     qr.save(filename)
     print('QR code saved as', filename)
@@ -23,11 +24,11 @@ def generate_qr(user, issuer, secret):
 def get_otp(secret):
     totp = pyotp.TOTP(secret)
     while True:
-        print("Current OTP:", totp.now())
+        print("Current One-time Password:", totp.now())
         time.sleep(30)
 
 def main():
-    # Parse command-line arguments
+    # parse command-line arguments
     if len(sys.argv) < 2:
         print('Usage: make run ARGS="--generate-qr" or make run ARGS="--get-otp"')
         return
@@ -37,7 +38,7 @@ def main():
     elif sys.argv[1] == "--get-otp":
         get_otp(secret_key)
     else:
-        print("Invalid command. Use --generate-qr or --get-otp.")
+        print('Invalid command. Use flags --generate-qr or --get-otp within command, make run ARGS="".')
 
 if __name__ == "__main__":
     main()
